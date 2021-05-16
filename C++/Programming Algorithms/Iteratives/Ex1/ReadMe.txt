44732794ac58e353eeb5f68e7d1147d1527841ba
Tyson Mendes Da Graça <79993852+tysonmdg@users.noreply.github.com>
2021-05-17 01:48:15 +0200

    Add files via upload

--- C++/Programming Algorithms/Iteratives/Ex1/ReadMe.txt
+++ C++/Programming Algorithms/Iteratives/Ex1/ReadMe.txt
@@ -4,3 +4,22 @@ You have to enter an integer number that indicates a number of test cases.
 On each case, you have to indicate the size of the vector and the position. Then, indicate the vector values.
 
 For each case, it indicates if its true or false the restriction (SI or NO).
+
+Example entry:
+3
+4 5
+3 6 10 20
+1 2 10 15 20
+0 2
+
+5 10
+3 3
+1 2 3
+4 5 6
+
+
+Example output:
+3 2 8 3
+3 1 2
+3 2 8 2
+0 0
--- C++/Programming Algorithms/Iteratives/Ex1/Source.cpp
+++ C++/Programming Algorithms/Iteratives/Ex1/Source.cpp
@@ -1,104 +1,82 @@
-// Nombre del estudiante: Tyson Mendes Da Gracia	
-// Usuario del juez: A47
+ï»¿// Nombre del alumno: Tyson Mendes  Da Gracia
+// Usuario del Juez: A47
+
 
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <vector>
 using namespace std;
-// Explicación del algoritmo utilizado
-// Comparas el número actual de cada vector y el menor lo almacenas en el vector solución.
-// En caso de que acabe uno, el while de el otro completa el vector solución.
-// Coste del algoritmo utilizado
-// O(n)
 
-std::vector<int> resolver(std::vector<int>const& v1, std::vector<int>const& v2) {
-	// Declaración de variables
-	// Codigo del alumno
-	int i = 0, j = 0, z = 0;
-	std::vector<int> sol;
 
-	while (i < v1.size() && j < v2.size()) {
+// funcioÌn que resuelve el problema
+bool resolver(vector<int> datos, int p) {
+	int max = datos[0], i = 1;
+	bool secumple = true;
 
-		// Aqui codigo del alumno
-		if (v1[i] < v2[j])
+	while(secumple && i < datos.size() && p < datos.size() - 1)
+	{
+		if (i <= p)
 		{
-			sol.push_back(v1[i]);
-			i++;
-			z++;
-		}
-		else if (v1[i] == v2[j])
-		{
-			sol.push_back(v1[i]);
-			i++;
-			j++;
-			z++;
+			if (datos[i] > max)
+			{
+				max = datos[i];
+			}
 		}
 		else
 		{
-			sol.push_back(v2[j]);
-			j++;
-			z++;
+			if(max >= datos[i])
+			{
+				secumple = false;
+			}
 		}
-
-	}
-	while (i < v1.size()) {
-		// Aqui codigo del alumno
-		sol.push_back(v1[i]);
 		i++;
-		z++;
-
 	}
-	while (j < v2.size()) {
-		// Aqui codigo del alumno
-		sol.push_back(v2[j]);
-		j++;
-		z++;
 
-	}
-	return sol;
+	return secumple;
 }
 
-
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuracioÌn, y escribiendo la respuesta
 void resuelveCaso() {
-	// Lectura de los datos de entrada
-	int n1, n2;
-	std::cin >> n1 >> n2;
-	std::vector<int> v1(n1);
-	std::vector<int> v2(n2);
-	for (int& i : v1) std::cin >> i;
-	for (int & j : v2) std::cin >> j;
-	// LLamada a la función que resuelve el problema
-	std::vector<int> sol = resolver(v1, v2);
-	// Escribir los resultados
-	if (!sol.empty()) {
-		std::cout << sol[0];
-		for (int i = 1; i < sol.size(); ++i)
-			std::cout << ' ' << sol[i];
+	// leer los datos de la entrada
+	int tam, p;
+	std::cin >> tam;
+	std::cin >> p;
+	vector<int> datos(tam);
+
+	for (int i = 0; i < datos.size(); i++)
+	{
+		std::cin >> datos[i];
 	}
-	std::cout << '\n';
+
+	bool sol = resolver(datos, p);
+	// escribir sol
+	if (sol) std::cout << "SI" << std::endl;
+	else std::cout << "NO" << std::endl;
+
 }
 
 int main() {
 	// Para la entrada por fichero.
 	// Comentar para acepta el reto
-
-#ifndef DOMJUDGE
-	std::ifstream in("sample04.in");
+/*#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
 	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
-#endif
+#endif */
+
 
 	int numCasos;
 	std::cin >> numCasos;
-	for (int i = 0; i < numCasos; ++i) {
+	for (int i = 0; i < numCasos; ++i)
 		resuelveCaso();
-	}
+
 
 	// Para restablecer entrada. Comentar para acepta el reto
-
-#ifndef DOMJUDGE // para dejar todo como estaba al principio
+/*#ifndef DOMJUDGE // para dejar todo como estaba al principio
 	std::cin.rdbuf(cinbuf);
 	system("PAUSE");
 #endif
-
+*/
 	return 0;
 }
